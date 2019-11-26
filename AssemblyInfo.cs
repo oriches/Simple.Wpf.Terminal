c54b2e8e3adc2010535d69c995102b40a2d64b3b
Ollie Riches <ollie_riches@hotmail.com>
2019-11-26 21:14:01 +0000

    Updated assembly info

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Simple.Wpf.Terminal")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
