29354f5fc6c386c43eb0e35b2cc8cef6e1a233ba
Ollie Riches <ollie_riches@hotmail.com>
2020-03-03 14:22:33 +0000

    Cleaned up project files for anutomated builds & package in AppVeyor

--- .net core/AssemblyInfo.cs
+++ .net core/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -8,3 +10,10 @@ using System.Windows;
     //(used if a resource is not found in the page,
     // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
